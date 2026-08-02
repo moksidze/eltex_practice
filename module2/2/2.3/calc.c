72676135f25284f67ea95066aa44a84bf9bd0027
moksidze <102008409+moksidze@users.noreply.github.com>
2026-08-02 21:31:50 +0700

    Add files via upload

---
+++ module2/2/2.3/calc.c
@@ -0,0 +1,21 @@
+#include "calc.h"
+
+double AdditionValues(double a, double b)
+{
+    return a + b;
+}
+
+double SubtractValues(double a, double b)
+{
+    return a - b;
+}
+
+double MultiplyValues(double a, double b)
+{
+    return a * b;
+}
+
+double DivideValues(double a, double b)
+{
+    return a / b;
+}
