02b2c9dfa4359af892c85d612a6fac1648fa181e
moksidze <102008409+moksidze@users.noreply.github.com>
2026-08-02 21:35:23 +0700

    Add files via upload

---
+++ module2/4/4.1/book.c
@@ -0,0 +1,205 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "book.h"
+
+contact *head = NULL;
+
+int AddContact(contact *newContact)
+{
+    contact *node;
+    contact *current;
+    contact *previous = NULL;
+
+    if (newContact == NULL)
+    {
+        return 0;
+    }
+
+    node = malloc(sizeof(contact));
+
+    if (node == NULL)
+    {
+        return 0;
+    }
+
+    *node = *newContact;
+
+    node->next = NULL;
+    node->prev = NULL;
+
+    current = head;
+
+    while (current != NULL)
+    {
+        int cmp = strcmp(current->surname, node->surname);
+
+        if (cmp < 0)
+        {
+            previous = current;
+            current = current->next;
+        }
+        else if (cmp == 0)
+        {
+            cmp = strcmp(current->name, node->name);
+
+            if (cmp < 0)
+            {
+                previous = current;
+                current = current->next;
+            }
+            else
+            {
+                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (previous == NULL)
+    {
+        node->next = head;
+        node->prev = NULL;
+
+        if (head != NULL)
+        {
+            head->prev = node;
+        }
+
+        head = node;
+    }
+    else
+    {
+        node->next = current;
+        node->prev = previous;
+
+        previous->next = node;
+
+        if (current != NULL)
+        {
+            current->prev = node;
+        }
+    }
+
+    return 1;
+}
+
+int EditContact(const char *name,
+                const char *surname,
+                int field,
+                const char *value,
+                int age)
+{
+    contact *current = head;
+
+    while (current != NULL)
+    {
+        if (strcmp(current->name, name) == 0 &&
+            strcmp(current->surname, surname) == 0)
+        {
+            break;
+        }
+
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        return 0;
+    }
+
+    switch (field)
+    {
+        case 1:
+            strcpy(current->name, value);
+            break;
+
+        case 2:
+            strcpy(current->surname, value);
+            break;
+
+        case 3:
+            strcpy(current->patronymic, value);
+            break;
+
+        case 4:
+            current->age = age;
+            break;
+
+        case 5:
+            strcpy(current->work, value);
+            break;
+
+        case 6:
+            strcpy(current->position, value);
+            break;
+
+        case 7:
+            strcpy(current->phone, value);
+            break;
+
+        case 8:
+            strcpy(current->email, value);
+            break;
+
+        case 9:
+            strcpy(current->link, value);
+            break;
+
+        case 10:
+            strcpy(current->messenger, value);
+            break;
+
+        default:
+            return -1;
+    }
+
+    return 1;
+}
+
+int DeleteContact(const char *name, const char *surname)
+{
+    contact *current = head;
+
+    while (current != NULL)
+    {
+        if (strcmp(current->name, name) == 0 &&
+            strcmp(current->surname, surname) == 0)
+        {
+            break;
+        }
+
+        current = current->next;
+    }
+
+    if (current == NULL)
+    {
+        return 0;
+    }
+
+    if (current->prev != NULL)
+    {
+        current->prev->next = current->next;
+    }
+    else
+    {
+        head = current->next;
+    }
+
+    if (current->next != NULL)
+    {
+        current->next->prev = current->prev;
+    }
+
+    free(current);
+
+    return 1;
+}
+
+contact *WatchContact(void)
+{
+    return head;
+}
---
+++ module2/4/4.1/main.c
@@ -0,0 +1,200 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "book.h"
+
+void Menu(void);
+
+int main(void)
+{
+    Menu();
+    return 0;
+}
+
+void Menu(void)
+{
+    int choice;
+
+    while (1)
+    {
+        contact newContact;
+        contact *current;
+
+        char name[100];
+        char surname[100];
+        char value[100];
+
+        int field;
+        int age = 0;
+
+        system("clear");
+
+        printf("*МЕНЮ*\n\n");
+        printf("1. Добавить контакт\n");
+        printf("2. Изменить контакт\n");
+        printf("3. Удалить контакт\n");
+        printf("4. Посмотреть список контактов\n");
+        printf("0. Выход\n\n");
+
+        printf("Выберите пункт меню: ");
+        scanf("%d", &choice);
+        while (getchar() != '\n');
+
+        switch (choice)
+        {
+        case 1:
+
+            printf("\nВведите имя: ");
+            fgets(newContact.name, sizeof(newContact.name), stdin);
+            newContact.name[strcspn(newContact.name, "\n")] = '\0';
+
+            printf("Введите фамилию: ");
+            fgets(newContact.surname, sizeof(newContact.surname), stdin);
+            newContact.surname[strcspn(newContact.surname, "\n")] = '\0';
+
+            printf("Введите отчество: ");
+            fgets(newContact.patronymic, sizeof(newContact.patronymic), stdin);
+            newContact.patronymic[strcspn(newContact.patronymic, "\n")] = '\0';
+
+            printf("Введите возраст: ");
+            scanf("%d", &newContact.age);
+            while (getchar() != '\n');
+
+            printf("Введите место работы: ");
+            fgets(newContact.work, sizeof(newContact.work), stdin);
+            newContact.work[strcspn(newContact.work, "\n")] = '\0';
+
+            printf("Введите должность: ");
+            fgets(newContact.position, sizeof(newContact.position), stdin);
+            newContact.position[strcspn(newContact.position, "\n")] = '\0';
+
+            printf("Введите телефон: ");
+            fgets(newContact.phone, sizeof(newContact.phone), stdin);
+            newContact.phone[strcspn(newContact.phone, "\n")] = '\0';
+
+            printf("Введите email: ");
+            fgets(newContact.email, sizeof(newContact.email), stdin);
+            newContact.email[strcspn(newContact.email, "\n")] = '\0';
+
+            printf("Введите ссылку: ");
+            fgets(newContact.link, sizeof(newContact.link), stdin);
+            newContact.link[strcspn(newContact.link, "\n")] = '\0';
+
+            printf("Введите мессенджер: ");
+            fgets(newContact.messenger, sizeof(newContact.messenger), stdin);
+            newContact.messenger[strcspn(newContact.messenger, "\n")] = '\0';
+
+            if (AddContact(&newContact))
+                printf("\nКонтакт успешно добавлен.\n");
+            else
+                printf("\nОшибка добавления контакта.\n");
+
+            break;
+
+        case 2:
+
+            printf("Введите имя контакта: ");
+            fgets(name, sizeof(name), stdin);
+            name[strcspn(name, "\n")] = '\0';
+
+            printf("Введите фамилию контакта: ");
+            fgets(surname, sizeof(surname), stdin);
+            surname[strcspn(surname, "\n")] = '\0';
+
+            printf("\nЧто изменить?\n");
+            printf("1. Имя\n");
+            printf("2. Фамилию\n");
+            printf("3. Отчество\n");
+            printf("4. Возраст\n");
+            printf("5. Место работы\n");
+            printf("6. Должность\n");
+            printf("7. Телефон\n");
+            printf("8. Email\n");
+            printf("9. Ссылку\n");
+            printf("10. Мессенджер\n");
+
+            scanf("%d", &field);
+            while (getchar() != '\n');
+
+            if (field == 4)
+            {
+                printf("Введите новый возраст: ");
+                scanf("%d", &age);
+                while (getchar() != '\n');
+
+                if (EditContact(name, surname, field, "", age))
+                    printf("Контакт изменен.\n");
+                else
+                    printf("Контакт не найден.\n");
+            }
+            else
+            {
+                printf("Введите новое значение: ");
+                fgets(value, sizeof(value), stdin);
+                value[strcspn(value, "\n")] = '\0';
+
+                if (EditContact(name, surname, field, value, 0))
+                    printf("Контакт изменен.\n");
+                else
+                    printf("Контакт не найден.\n");
+            }
+
+            break;
+
+        case 3:
+
+            printf("Введите имя: ");
+            fgets(name, sizeof(name), stdin);
+            name[strcspn(name, "\n")] = '\0';
+
+            printf("Введите фамилию: ");
+            fgets(surname, sizeof(surname), stdin);
+            surname[strcspn(surname, "\n")] = '\0';
+
+            if (DeleteContact(name, surname))
+                printf("Контакт удален.\n");
+            else
+                printf("Контакт не найден.\n");
+
+            break;
+
+        case 4:
+
+            current = WatchContact();
+
+            if (current == NULL)
+            {
+                printf("Телефонная книга пуста.\n");
+            }
+            else
+            {
+                while (current != NULL)
+                {
+                    printf("\nИмя: %s\n", current->name);
+                    printf("Фамилия: %s\n", current->surname);
+                    printf("Отчество: %s\n", current->patronymic);
+                    printf("Возраст: %d\n", current->age);
+                    printf("Место работы: %s\n", current->work);
+                    printf("Должность: %s\n", current->position);
+                    printf("Телефон: %s\n", current->phone);
+                    printf("Email: %s\n", current->email);
+                    printf("Ссылка: %s\n", current->link);
+                    printf("Мессенджер: %s\n", current->messenger);
+
+                    current = current->next;
+                }
+            }
+
+            break;
+
+        case 0:
+            return;
+
+        default:
+            printf("Неверный пункт меню.\n");
+        }
+
+        printf("\nНажмите Enter...");
+        getchar();
+    }
+}
